1e96c3ac282c1684968a525ed2501b8ea1e4f978
Paarxomets <parxomez1@mail.ru>
2026-03-03 15:50:04 +0300

    get start all pages

--- src/javascript/modules/header.js
+++ src/javascript/modules/header.js
@@ -1,21 +1,21 @@
 export function GetHeader (){
     const header = document.querySelector('.header_container')
     const htmlHeader = `
-    <div class="main_header_positon">
+            <div class="main_header_positon">
                 <div class="headerfix">
                     <div  class="header-container stroke_main">
                         <div class="header-content">
                             <nav class="navigation">
                                 <li><a href="index.html"><h4>главная</h4></a></li>
-                                <li><a href="index.html#services"><h4>Услуги</h4></a></li>
+                                <li><a href="index.html#services"><h4>услуги</h4></a></li>
                                 <li><a href="index.html#about"><h4>о нас</h4></a></li>
                                 <li><a href="#footer"><h4>котнтакты</h4></a></li>
                             </nav>
-                            <a  href="index.html" class="logo"><img src="./src/assests/img/main_logo.jpg" alt="логотип барбершопа Flavus"></a>
+                            <a  href="index.html" class="logo"><img src="/src/assests/img/main_logo.jpg" alt="логотип барбершопа Flavus"></a>
                             <nav class="navigation">
-                                <li><a href="education.html"><h4>Академия</h4></a></li>
-                                <li><a href="vacancy.html"><h4>Вакансии</h4></a></li>
-                                <li><a href="lookbook.html"><h4>Lookbook</h4></a></li>
+                                <li><a href="education.html"><h4>академия</h4></a></li>
+                                <li><a href="vacancy.html"><h4>вакансии</h4></a></li>
+                                <li><a href="lookbook.html"><h4>lookbook</h4></a></li>
                             </nav>
                             <div class="burgerbutton">
                                 <input class="burgerclose" type="checkbox" id="checkbox">
@@ -32,12 +32,12 @@ export function GetHeader (){
                     <div class="header_mobile stroke_main">
                         <nav class="mobile">
                             <li><a href="index.html"><h4>главная</h4></a></li>
-                            <li><a href="index.html#services"><h4>Услуги</h4></a></li>
+                            <li><a href="index.html#services"><h4>услуги</h4></a></li>
                             <li><a href="index.html#about"><h4>о нас</h4></a></li>
                             <li><a href="#footer"><h4>котнтакты</h4></a></li>
-                            <li><a href="education.html"><h4>Академия</h4></a></li>
-                            <li><a href="vacancy.html"><h4>Вакансии</h4></a></li>
-                            <li><a href="lookbook.html"><h4>Lookbook</h4></a></li>
+                            <li><a href="education.html"><h4>академия</h4></a></li>
+                            <li><a href="vacancy.html"><h4>вакансии</h4></a></li>
+                            <li><a href="lookbook.html"><h4>lookbook</h4></a></li>
                         </nav>
                     </div>
                 </div>
